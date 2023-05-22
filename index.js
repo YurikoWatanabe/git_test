545d8a9e3c374ce876bcb7458b04ed0f1b7e6ea4
YurikoWatanabe <yuri0620yuri0620@gmai.com>
2023-05-23 00:10:58 +0900

    hiddenmenu

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 'use strict';
+//ハンバーガーが押されたら
+$(document).ready(function(){
+    $('.hamburger').on('click', function(){
+        $('.bar1, .bar2, .bar3').toggleClass('open');
+        //メニュー開閉
+        $('.menu').toggleClass('show');       
+    }); 
+});
 
